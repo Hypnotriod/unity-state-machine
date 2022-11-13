f413d8dee5d6516fa9c12711c853fb030351c6e5
ipikin <illia.pikin@gmail.com>
2022-11-13 12:15:56 -0800

    update action handle behavior, refactoring

--- Assets/Scripts/Components/ComponentWithActions.cs
+++ Assets/Scripts/Components/ComponentWithActions.cs
@@ -1,32 +1,25 @@
 ﻿using Assets.Scripts.StateMachine;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 internal class ComponentWithActions : GameComponent
 {
-
     public ActionHandler ActionInstant()
     {
         Debug.LogFormat("Action on {0}: Instant", gameObject.name);
         return CompletedHandler();
     }
 
-    private readonly ActionHandler actionDelayedHandler = new();
+    private readonly ActionHandlersQueue actionDelayed = new();
     public ActionHandler ActionDelayed(float seconds)
     {
         Debug.LogFormat("Action on {0}: Wait for {1} seconds", gameObject.name, seconds);
         Coroutine delayCoroutine = Delay(seconds, () =>
         {
             Debug.LogFormat("Action on {0}: Completed after {1} seconds", gameObject.name, seconds);
-            actionDelayedHandler.Complete();
+            actionDelayed.Complete();
         });
 
-        return actionDelayedHandler.Set().WithAbort(() =>
+        return actionDelayed.Enqueue().WithAbort(() =>
         {
             Debug.LogFormat("Action on {0}: Wait for {1} seconds: Aborted", gameObject.name, seconds);
             StopCoroutine(delayCoroutine);
---
+++ Assets/Scripts/StateMachine/ActionHandlersQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.StateMachine
+{
+    public class ActionHandlersQueue
+    {
+        private readonly Queue<ActionHandler> actionHandlers = new();
+
+        public ActionHandler Enqueue()
+        {
+            ActionHandler handler = new();
+            actionHandlers.Enqueue(handler);
+            return handler;
+        }
+
+        public void Complete()
+        {
+            if (actionHandlers.Count == 0) { return; }
+            actionHandlers.Dequeue().Complete();
+        }
+
+        public void Abort()
+        {
+            if (actionHandlers.Count == 0) { return; }
+            actionHandlers.Dequeue().Abort();
+        }
+    }
+}
